fbbfe2ea5555e0b8566780b2f71cdde2dfb4e16b
spring-hh <spring.hh.shu@gmail.com>
2022-11-08 22:43:08 +0900

    next-authの追加

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,14 @@
+import CognitoProvider from 'next-auth/providers/cognito';
 import Head from 'next/head';
 import Image from 'next/image';
+
+providers: [
+  CognitoProvider({
+    clientId: process.env.COGNITO_CLIENT_ID ? process.env.COGNITO_CLIENT_ID : '',
+    clientSecret: process.env.COGNITO_CLIENT_SECRET ? process.env.COGNITO_CLIENT_SECRET : '',
+    issuer: process.env.COGNITO_ISSUER
+  })
+];
 
 export default function Home() {
   return (
